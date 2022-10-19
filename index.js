5c1850f5335839b436b0734c201a469b59ac5fc6
Tahsan <tahsanknmas14@gmail.com>
2022-10-19 18:04:26 +0600

    dotenv setupp

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+
 const port = process.env.PORT || 5000;
 
-//dbuser :dbusertahsan
-//pass: i7IM9cBas3TaLw0l
-
 //middleware
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello mama.whats up!')
@@ -42,7 +41,7 @@ app.listen(port, () => {
 })
 
 
-const uri = "mongodb+srv://dbusertahsan:i7IM9cBas3TaLw0l@cluster0.v7opeaw.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7opeaw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
