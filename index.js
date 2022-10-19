a72a9fa99a79b4f6739cb5f68b037be4e64e8fb3
Tahsan <tahsanknmas14@gmail.com>
2022-10-19 18:32:17 +0600

    added get method of perfume

--- index.js
+++ index.js
@@ -43,9 +43,21 @@ app.listen(port, () => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7opeaw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const perfumeCollection = client.db("perfumeWareHouse").collection('perfume');
+
+        app.get('/perfume', async (req, res) => {
+            const query = {};
+            const cursor = perfumeCollection.find(query);
+            const perfume = await cursor.toArray();
+            res.send(perfume);
+        })
+    }
+    finally {
+        //client.connect()
+    }
+};
+run().catch(console.dir);
